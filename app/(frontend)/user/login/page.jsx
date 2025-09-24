19a63319159776b7ea4120d99cc214d231671b25
sai deekshith <saideekshith06@gmail.com>
2025-09-24 12:06:33 +0530

    all over

--- app/(frontend)/user/login/page.jsx
+++ app/(frontend)/user/login/page.jsx
@@ -25,6 +25,7 @@ export default function SignIn() {
     if (result?.error) {
       setError("Invalid email or password");
     } else {
+      localStorage.setItem("userEmail", "true");
       router.push("/");
     }
   };
