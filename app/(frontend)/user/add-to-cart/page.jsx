779e20a052ae88f16e1395351bc9f8d7f3a11624
sai deekshith <saideekshith06@gmail.com>
2025-09-24 12:50:22 +0530

    lawda

--- app/(frontend)/user/add-to-cart/page.jsx
+++ app/(frontend)/user/add-to-cart/page.jsx
@@ -25,7 +25,7 @@ export default function AddToCartPage() {
   useEffect(() => {
     if (status === "unauthenticated") {
       alert("Please login first");
-      router.push("/signin");
+      router.push("/user/signin");
     }
   }, [status, router]);
 
